818c142da4dc02d2244781efa72ffd2050c84ad5
Rodel Aragon <34167795+zephyr9990@users.noreply.github.com>
2019-11-20 14:54:23 -0800

    Added content packs,  a default avatar, and maps

---
+++ Unreal_Aragon_RPG/Source/Unreal_Aragon_RPG/Private/Avatar.cpp
@@ -0,0 +1,74 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Components/InputComponent.h"
+#include "Engine/World.h"
+#include "Avatar.h"
+
+// Sets default values
+AAvatar::AAvatar()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AAvatar::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AAvatar::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AAvatar::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("Forward", this, &AAvatar::MoveForward);
+	PlayerInputComponent->BindAxis("Strafe", this, &AAvatar::MoveRight);
+	PlayerInputComponent->BindAxis("Pitch", this, &AAvatar::RotatePitch);
+	PlayerInputComponent->BindAxis("Yaw", this, &AAvatar::RotateYaw);
+}
+
+void AAvatar::MoveForward(float amount)
+{
+	// Don't enter the body of this function if Controller is not set up yet,
+	// or if the amount to move is equal to 0.
+	if (Controller && amount)
+	{
+		FVector forward = GetActorForwardVector();
+		// we call AddMovementInput to actually move the
+		// player by "amount" in the "forward" direction
+		AddMovementInput(forward, amount);
+	}
+}
+
+void AAvatar::MoveRight(float amount)
+{
+	// Don't enter the body of this function if Controller is not set up yet,
+	// or if the amount to move is equal to 0.
+	if (Controller && amount)
+	{
+		FVector right = GetActorRightVector();
+		// we call AddMovementInput to actually move the
+		// player by "amount" in the "forward" direction
+		AddMovementInput(right, amount);
+	}
+}
+
+void AAvatar::RotateYaw(float amount)
+{
+	AddControllerYawInput(LookSensitivity * amount * GetWorld()->GetDeltaSeconds());
+}
+
+void AAvatar::RotatePitch(float amount)
+{
+	AddControllerPitchInput(LookSensitivity * amount * GetWorld()->GetDeltaSeconds());
+}
+
---
+++ Unreal_Aragon_RPG/Source/Unreal_Aragon_RPG/Public/Avatar.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Avatar.generated.h"
+
+UCLASS()
+class UNREAL_ARAGON_RPG_API AAvatar : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AAvatar();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// Move player forward
+	void MoveForward(float amount);
+
+	// Move player right
+	void MoveRight(float amount);
+
+	// Controls player yaw 
+	void RotateYaw(float amount);
+
+	// Controls player pitch
+	void RotatePitch(float amount);
+
+private:
+	float LookSensitivity = 200.0f;
+};
